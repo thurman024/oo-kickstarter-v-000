2cc95467c3ee00bf62c3b056b8fdfa38837d42b2
Stephen Thurman <thurman024@gmail.com>
2018-01-31 02:49:39 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,10 +3,10 @@ class Backer
 
   def initialize(name)
     @name = name
-    backed_projects = []
+    @backed_projects = []
   end
 
   def back_project(project)
-    backed_projects << project
+    @backed_projects << project
   end
 end
